ab56c07bee59a2652283490fd394050425b8dca2
Derek Jones <djamesjones4@gmail.com>
2018-01-01 16:57:20 -0700

    added border radius to header and CNAME file

--- index.js
+++ index.js
@@ -1,61 +1,71 @@
-var body = document.getElementsByTagName('body')[0];
-var boxContainer = document.createElement('div');
+const body = document.getElementsByTagName('body')[0];
+const boxContainer = document.createElement('div');
+
 boxContainer.className = "container";
 boxContainer.id = "canvas";
 body.appendChild(boxContainer)
 
-for (let i = 0; i < 621; i++) {
-    var div = document.createElement('div')
-    boxContainer.appendChild(div)
-    div.className = "pixel"
+for (i = 0; i < 621; i++) {
+  let div = document.createElement('div')
+  boxContainer.appendChild(div)
+  div.className = "pixel"
 }
 
-var palette = document.createElement('div');
+let palette = document.createElement('div');
 palette.className = "palette";
 palette.id = 'palette-id'
 body.appendChild(palette);
 
-var palletteColor = ['red', '#EA882C', 'rgb(38, 140, 162)', '#50d536', 'indigo', '#21e8ee'];
+let palletteColor = [
+  'red',
+  '#EA882C',
+  'rgb(38, 140, 162)',
+  '#50d536',
+  'indigo',
+  '#21e8ee',
+  '#ffffff',
+  'rgb(232, 143, 165)'
+];
 
-for (var i = 0; i <= 5; i++) {
-    let div = document.createElement('div');
-    div.className = 'colors ';
-    div.className += palletteColor[i];
-    div.style.backgroundColor = palletteColor[i];
-    palette.appendChild(div);
+for (let i = 0; i <= 7; i++) {
+  let div = document.createElement('div');
+  div.className = 'colors ';
+  div.className += palletteColor[i];
+  div.style.backgroundColor = palletteColor[i];
+  palette.appendChild(div);
 }
 
-var selectedColor = {
-    color: ''
+let selectedColor = {
+  color: ''
 }
 palette.addEventListener('click', function() {
-    if (event.target.id !== 'palette-id') {
-        selectedColor.color = event.target.style.backgroundColor
-    }
-    console.log(selectedColor);
+  if (event.target.id !== 'palette-id') {
+    selectedColor.color = event.target.style.backgroundColor
+  }
+  console.log(selectedColor);
 })
-var mouseDown = false;
-// var mouseLeave = false;
+let mouseDown = false;
+// let mouseLeave = false;
 boxContainer.addEventListener('mousedown', function() {
-    mouseDown = true;
+  mouseDown = true;
 })
 boxContainer.addEventListener('mouseup', function() {
-    mouseDown = false;
+  mouseDown = false;
 })
 boxContainer.addEventListener('mouseover', function() {
-    // mouseLeave = true;
-    if (event.target.id !== "canvas" && mouseDown === true) {
-        event.target.style.backgroundColor = selectedColor['color'];
-    }
+  // mouseLeave = true;
+  if (event.target.id !== "canvas" && mouseDown === true) {
+    event.target.style.backgroundColor = selectedColor['color'];
+  }
+
 })
 boxContainer.addEventListener('click', function() {
-    if (event.target.id !== 'canvas') {
-        event.target.style.backgroundColor = selectedColor['color'];
-    }
+  if (event.target.id !== 'canvas') {
+    event.target.style.backgroundColor = selectedColor['color'];
+  }
 })
 
-//toggle class on click event so when clicked again the class and color of the pixel will revert to white
-
+// toggle class on click event so when clicked again the class and color of the pixel will revert to white
 
 // boxContainer.addEventListener('mouseleave', function() {
 //     if (event.target.id === 'canvas')
